f9dd33943b80f3688f888c315403ccda175435d6
ibrahim <humazed@gmail.com>
2019-08-22 13:23:49 +0200

    ##  2.5.0
    * Added materialRoute to navigation.dart.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+##  2.5.0
+
+* Added materialRoute to navigation.dart.
+
 ##  2.4.0
 
 * Update deps.
--- lib/src/navigation.dart
+++ lib/src/navigation.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
 
-Future<T> push<T extends Object>(BuildContext context, Widget widget) =>
-    Navigator.push(context, MaterialPageRoute(builder: (context) => widget));
+Future push(BuildContext context, Widget widget) =>
+    Navigator.push(context, materialRoute(widget));
+
+MaterialPageRoute materialRoute(Widget widget) =>
+    MaterialPageRoute(builder: (context) => widget);
